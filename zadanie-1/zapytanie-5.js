53944eed5307ebf9f1198a73f24ec0596297df0f
Bartosz Żuk <bartoszzuk.poczta@gmail.com>
2021-10-14 13:06:03 +0200

    Dodano zapytanie 5

---
+++ zadanie-1/zapytanie-5.js
@@ -0,0 +1,5 @@
+result = db.people.find(
+    { "birth_date": { "$gte": "2000-01-01T00:00:00Z" }},
+    { "_id": false, "first_name": true, "last_name": true, "location.city": true }
+)
+printjson(result)
