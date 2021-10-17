83fbae1d84df0bbc6bb192f44b95616be62f79cd
Bartosz Żuk <bartoszzuk.poczta@gmail.com>
2021-10-17 14:32:54 +0200

    Poprawka w skrypcie

--- zadanie-1/run-query.sh
+++ zadanie-1/run-query.sh
@@ -2,7 +2,7 @@
 
 QUERY_FILE=$1
 FILE_NAME=${QUERY_FILE%%\.js}
-QUERY_NUMBER=${FILE_NAME##*-}
+QUERY_NUMBER=${FILE_NAME#*-}
 
 mongosh --quiet nbd "${QUERY_FILE}" > "./wynik-${QUERY_NUMBER}.json"
 echo "Saved results of query to wynik-${QUERY_NUMBER}.json"
